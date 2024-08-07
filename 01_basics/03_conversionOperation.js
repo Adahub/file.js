facced41dcb24d05793c322492b7d6031c565d7c
Adahub <75196033+Adahub@users.noreply.github.com>
2024-08-07 08:36:05 +0000

    conversion datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+//let age = 23
+//let age = null
+//let age = "23"
+// let age = "23ads"
+// console.log(typeof age)
+// console.log(typeof (age))
+
+// let valueInNumber = Number(age)
+// console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//"23" => 23
+// "23ads" => NaN
+// true => 1; false => 0
+
+//let isLoggedIn = 1
+//let isLoggedIn = ""
+let isLoggedIn = "ada"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
