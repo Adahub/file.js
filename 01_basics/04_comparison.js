0d4a916dbffb3c6782cb991191fdf1262bb123e2
Adahub <75196033+Adahub@users.noreply.github.com>
2024-08-08 07:05:17 +0000

    comparison b/t datatypes

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,31 @@
+// console.log(2>1)
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log( 2== 1);
+// console.log(2 != 1);
+ 
+//Note: avoid these types of code for comparison
+
+//console.log("4">1);
+//console.log("06">7);
+
+// console.log(null < 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+// console.log(null <= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+//********************STRICT CHECK*************/
+
+console.log("2" === 2);
+
+
+
+
+
+
+
+
